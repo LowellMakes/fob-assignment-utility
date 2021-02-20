de7e8eca6b0ffcb9cfe936445c430c59d8b8ae32
Ben Brown <brown@pop-os.localdomain>
2021-02-19 20:17:41 -0500

    Version 2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter
 import requests
 import pandas as pd
 from tkinter import ttk
+from tkinter import messagebox
 import time
 
 class AutocompleteCombobox(ttk.Combobox):
@@ -65,32 +66,50 @@ class SignUpAll():
 
         self.top = master
         self.users = None
+        self.doorflow_auth = requests.auth.HTTPBasicAuth('3zxv-BQUSiSyER5UxrM4','x')
+        self.nexudus_auth = ()
 
-        nameLabel = tkinter.Label(self.top, text='Select Your Name')
+        self.createStartScreen()
+        
+    def createStartScreen(self):
+
+        self.startScreen = tkinter.Toplevel(self.top)
+
+        titleLabel = tkinter.Label(self.startScreen, text='Create A New Fob')
+        titleLabel.grid(row=1, column=1)
+
+        startButton = tkinter.Button(self.startScreen, text='Start', command=self.createInitilizeScreen)
+        startButton.grid(row=2, column=1)
+
+    def createInitilizeScreen(self):
+
+        self.startScreen.destroy()
+        self.initScreen = tkinter.Toplevel(self.top)
+
+        nameLabel = tkinter.Label(self.initScreen, text='Select Your Name')
         nameLabel.grid(row=1, column=1, columnspan=2)
 
         self.namesList = self.getNamesFromNexudus()
         self.selectedName = tkinter.StringVar(self.top)
-
-        nameDropDown = AutocompleteCombobox(self.top, textvariable=self.selectedName)
+        
+        nameDropDown = AutocompleteCombobox(self.initScreen, textvariable=self.selectedName)
         nameDropDown.set_completion_list(self.namesList)
         nameDropDown.grid(row=2, column=1, columnspan=2)
 
-        selectButton = tkinter.Button(self.top, text='Start', command=self.startAssignment)
+        selectButton = tkinter.Button(self.initScreen, text='Start', command=self.startAssignment)
         selectButton.grid(row=3, column=1)
 
-        refreshButton = tkinter.Button(self.top, text='Refresh', command = self.getNamesFromNexudus)
+        refreshButton = tkinter.Button(self.initScreen, text='Refresh', command = self.getNamesFromNexudus)
         refreshButton.grid(row=3, column=2)
 
     def getNamesFromNexudus(self):
         """Pull the list of users from nexudus and return a list of the names"""
-        nexudus_auth = ('', b'')
+        
         url = "https://spaces.nexudus.com/api/spaces/coworkers?size=500"
-        all_users = requests.get(url, auth=nexudus_auth)
+        all_users = requests.get(url, auth=self.nexudus_auth)
         all_users = pd.DataFrame(all_users.json()['Records'])
         names = all_users.FullName.to_list()
-        all_users = None
-        names = ['name1','name2','name3']
+
         self.users = all_users
 
         return names
@@ -98,31 +117,99 @@ class SignUpAll():
     def startAssignment(self):
         """Wait for a signal from DoorFlow and assign the fob number to the appropriate user"""
         name = self.selectedName.get()
-        self.statusWindow = tkinter.Toplevel(self.top)
-        self.statusWindow.configure(bg='green')
-        waitLabel = tkinter.Label(self.statusWindow, text='Hit Fob against the reader now')
-        waitLabel.grid()
 
-        self.doorflowStatus = False
+        tkinter.messagebox.showinfo(self.initScreen, message='Hit Card Now, you will hear a beep after your badge is read')
 
-        self.checkDoorFlow()
+        lastevent = self.getDoorFlowEvents()
 
-    def checkDoorFlow(self):
+        if lastevent == False:
+            print('You didnt scan a card, try again')
+            self.initScreen.destroy()
+            self.createInitilizeScreen()
+            return
 
-        if self.doorflowStatus == False:
-            doorflow_auth = requests.auth.HTTPBasicAuth('3zxv-BQUSiSyER5UxrM4','x')
-            time.sleep(2)
-            print('Response Recieved')
-            self.statusWindow.destroy()
-            self.doorflowStatus = True
-        elif self.doorflowStatus == True:
-            print('Do Nothing')
+        status = self.searchNexudusCards(lastevent)
 
-        self.top.after(2000, self.checkDoorFlow)
+        # Check if card is already in use by someone else
+        if status == False:
+            print('That card is already in use, try again')
+            self.initScreen.destroy()
+            self.createInitilizeScreen()
+            return
 
+        # TODO: Check time stamp and make sure it makes sense
 
+        # TODO: If everything is ok, take the user name and the number and push to Nexudus
+        status = self.pushUpdate(lastevent,name)
+        if status == False:
+            self.initScreen.destroy()
+            self.createInitilizeScreen()
+            return
+        else:
+            self.initScreen.destroy()
+            self.createStartScreen()
+
+    def getDoorFlowEvents(self):
+        url = "https://admin.doorflow.com/api/2/events?n=20"
+        all_events = requests.get(url, auth=self.doorflow_auth)
+        
+        return all_events.json()[0]
+
+        for event in all_events.json():
+            if event['door_controller_name'] == 'Design Point':
+                return event
+            else:
+                return False
+
+    def searchNexudusCards(self,lastevent):
+
+        cardNumber = int(lastevent['credentials_number'])
+
+        if cardNumber in self.cleanCredentialList(self.users.AccessCardId):
+            return False
+
+        if cardNumber in self.cleanCredentialList(self.users.KeyFobNumber):
+            return False
+
+        return True
+
+    def cleanCredentialList(self,series):
+
+        list_of_creds = []
+        for value in series.to_list():
+
+            if value == None:
+                continue
+            elif '\t' in value:
+                n = int(value.split('\t')[-1])
+                list_of_creds.append(n)
+            elif ',' in value:
+                for n in value.split(','):
+                    list_of_creds.append(int(n))
+            else:
+                try:
+                    list_of_creds.append(int(value))
+                except:
+                    pass
+
+        return(list_of_creds)
+
+    def pushUpdate(self,lastevent,name):
+
+        update = self.users[self.users.FullName==name].iloc[0].to_dict()
+        update['KeyFobNumber'] = update['KeyFobNumber']+','+lastevent['credentials_number']
+
+        url = "https://spaces.nexudus.com/api/spaces/coworkers"
+        r = requests.put(url, auth=self.nexudus_auth, json=update)
+        if r.status_code == 201:
+            print('Success')
+            return True
+        else:
+            print('Something went wrong with the upload')
+            return False
 
 if __name__ == '__main__':
     root = tkinter.Tk()
+    root.withdraw()
     app = SignUpAll(root)
     root.mainloop()
